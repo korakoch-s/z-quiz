8091c26828c6f62911d453712da0fb0ae0d4347c
Korakoch Singkam <korakoch.s@gmail.com>
2017-07-22 19:13:41 +0700

    Draft register page.

--- z-quiz.ngApp/src/app/app.module.ts
+++ z-quiz.ngApp/src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,18 +12,20 @@ import { SummaryComponent } from './components/summary/summary.component';
 import { QuizItemComponent } from './components/quiz-item/quiz-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent,
-    QuizComponent,
-    SummaryComponent,
-    QuizItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RegisterComponent,
+        QuizComponent,
+        SummaryComponent,
+        QuizItemComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ModalModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- z-quiz.ngApp/src/app/components/register/register.component.ts
+++ z-quiz.ngApp/src/app/components/register/register.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    public userName: string;
+    public modalRef: BsModalRef;
 
-  constructor() { }
+    constructor(private router: Router, private modalService: BsModalService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    goClick(template: TemplateRef<any>) {
+        if (!this.userName || this.userName.length <= 0) {
+            console.log('Please input username before go...');
+            this.openAlertModal(template);
+            event.stopPropagation();
+        } else {
+            this.router.navigate(['/quiz', this.userName]);
+        }
+    }
+
+    public openAlertModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template);
+    }
 
 }
